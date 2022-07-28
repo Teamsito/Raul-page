0290f5b712f94354f51e030017a96a38226df92e
manguila2525 <manguilas2525@gmail.com>
2022-07-28 01:30:11 -0400

    RP-001 - Banner header

---
+++ src/components/molecules/Banner/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../../../assets/Logo.png';
+import {MainBanner, ImgLogo, Information} from './style';
+
+const Index = () => {
+    return (
+        <>
+            <MainBanner>
+                <Information>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam perspiciatis possimus quisquam voluptas consequuntur eos reiciendis .
+                </Information>
+                <ImgLogo src={Logo} alt="ADMINISTRACION RJ" />
+            </MainBanner>
+        </>
+    );
+}
+
+export default Index;
